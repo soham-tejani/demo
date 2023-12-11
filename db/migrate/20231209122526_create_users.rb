40813307c50926373ca29401804d0a62e4fd43c9
soham-tejani <sametejani123@gmail.com>
2023-12-11 10:58:51 +0530

    Add devise and user module

--- db/migrate/20231209122526_create_users.rb
+++ db/migrate/20231209122526_create_users.rb
@@ -3,9 +3,6 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password
-      t.string :password_confirmation
       t.string :first_name
       t.string :last_name
       t.bigint :phone
